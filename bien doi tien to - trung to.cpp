cc7799b99e1fa3424912d27e817ad9b88aab78c6
tuấn naive <99082087+vantuan2905@users.noreply.github.com>
2022-06-23 23:19:08 +0700

    Add files via upload

---
+++ bien doi tien to - trung to.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+main(){
+	int t;
+	cin>>t;
+	while(t--){
+		string s;
+		cin>>s;
+		stack<string> st;
+		for(int i=s.length()-1;i>=0;i--){
+			if(s[i]=='+'||s[i]=='-'||s[i]=='*'||s[i]=='/'){
+				string a=st.top();st.pop();string b=st.top();st.pop();
+				string ss="("+a+s[i]+b+")";
+				st.push(ss);
+			}else st.push(string(1,s[i]));
+		}
+		cout<<st.top()<<endl;
+	}
+}
